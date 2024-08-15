f7546c458023fdbd47feb5db7d61a5dfce0a917e
Nicolas Fernandez Riesen <riesenfnicolas@gmail.com>
2024-08-15 17:53:05 -0300

    New cookie for store the id & change the returns por the post http methods, Add edit user view without css & the api post action functionality, Add sSweetAlert2 to the proyect & 3 function to call it with 3 types of alerts, create js to edit, create & login user

---
+++ BudgetTracker/wwwroot/js/Home/Create.js
@@ -0,0 +1,27 @@
+﻿async function CreateUser() {
+    const form = document.getElementById('createUserForm');
+    const formData = new FormData(form);
+
+    const loadingSwal = showLoadingAlert('Creando usuario');
+
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
+
+    try {
+        const response = await fetch('/Home/Create', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            await showSuccessAlert('¡Éxito!', 'Usuario creado correctamente.');
+
+            window.location.href = '/Home';
+        } else {
+            throw new Error('Hubo un problema a la hora de crear el usuario. Vuelve a intentarlo.');
+        }
+    } catch (error) {
+        await showErrorAlert("Error", error.message);
+    } finally {
+        Swal.close();
+    }
+}
---
+++ BudgetTracker/wwwroot/js/User/Edit.js
@@ -0,0 +1,46 @@
+﻿document.getElementById('editOptions').addEventListener('change', function () {
+    const selectedOption = this.value;
+    const sections = document.querySelectorAll('.edit-section');
+    const submitBtn = document.getElementById('submitBtn');
+
+    // Hide all sections
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Show the selected section
+    if (selectedOption) {
+        document.getElementById(selectedOption + 'Section').style.display = 'block';
+        submitBtn.style.display = 'block';
+    } else {
+        submitBtn.style.display = 'none';
+    }
+});
+
+async function sendEditUser() {
+    const form = document.getElementById('editUserForm');
+    const formData = new FormData(form);
+
+    const loadingSwal = showLoadingAlert();
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    try {
+        const response = await fetch('/User/Edit', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            await showSuccessAlert("Guardado", "Los cambios se guardaron con exito.");
+
+            window.location.href = '/Home';
+        } else {
+            throw new Error('Error al guardar los datos. Intentelo de nuevo.');
+        }
+    } catch (error) {
+        await showErrorAlert("Error", error.message);
+    } finally {
+        Swal.close();
+    }
+}
